26f79e43a022d0a339a6f51b8be82d20f3714a4a
PrimaryBelman <113471529+PrimaryBelman@users.noreply.github.com>
2022-11-02 12:45:52 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have replaced every mean function reference to "inverse"
+## I have given my own input 
+##solved for my function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(inverse) m <<- inverse
+        getmean <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## I have just changed the references of mean to inverse and kept remaining as is
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inverse(data, ...)
+        x$setInverse(m)
+        m
+}
 }
